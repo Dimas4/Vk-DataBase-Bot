7e52dc474401c043ccb419a241ec8b234aa88ed2
Ivan <ivanshatukho@yandex.ru>
2018-10-11 13:15:34 +0300

    try except

--- bot/bot.py
+++ bot/bot.py
@@ -22,41 +22,45 @@ def start(token, backend, db, dialog, filename):
         if messages["count"] >= 1:
             id, message_id, body, url = bot.get_message_ids_image(messages)
             print('Запрос:', id, body)
-            if url and url[-3:] == 'jpg':
-                urllib.request.urlretrieve(url, filename)
-                db.save_image(id, body, filename)
-                bot.send_message(id, dialog.ok)
-                continue
+            try:
 
-            if body.lower() in dialog.common_answer:
-                bot.send_message(id, dialog.common_answer[body.lower()])
-                continue
+                if url and url[-3:] == 'jpg':
+                    urllib.request.urlretrieve(url, filename)
+                    db.save_image(id, body, filename)
+                    bot.send_message(id, dialog.ok)
+                    continue
 
-            for key in dialog.action:
-                if key in body.lower():
+                if body.lower() in dialog.common_answer:
+                    bot.send_message(id, dialog.common_answer[body.lower()])
+                    continue
 
-                    part = body.lower().split(':')[1].strip()
-                    if '&' in body.lower():
-                        splitted = part.split('&')
-                        part_1, part_2 = splitted[0].strip(), splitted[1].strip()
-                        db.hset(id, part_1, part_2)
-                        bot.send_message(id, dialog.ok)
+                for key in dialog.action:
+                    if key in body.lower():
+
+                        part = body.lower().split(':')[1].strip()
+                        if '&' in body.lower():
+                            splitted = part.split('&')
+                            part_1, part_2 = splitted[0].strip(), splitted[1].strip()
+                            db.hset(id, part_1, part_2)
+                            bot.send_message(id, dialog.ok)
+                            break
+
+                        data = db.hget(id, part)
+
+                        if data and len(data) > 15000:
+                            with open(filename, 'wb') as file:
+                                file.write(data)
+                            bot.upload_image(id, filename)
+                            break
+                        elif data:
+                            bot.send_message(id, data)
+                        else:
+                            bot.send_message(id, dialog.text_does_not_exist)
                         break
-
-                    data = db.hget(id, part)
-
-                    if data and len(data) > 15000:
-                        with open(filename, 'wb') as file:
-                            file.write(data)
-                        bot.upload_image(id, filename)
-                        break
-                    elif data:
-                        bot.send_message(id, data)
-                    else:
-                        bot.send_message(id, dialog.text_does_not_exist)
-                    break
-            else:
-                bot.send_message(id, dialog.don_t_understand)
-
-
+                else:
+                    bot.send_message(id, dialog.don_t_understand)
+            except Exception as e:
+                print(str(e))
+                bot.send_message(id, dialog.error)
+            
         time.sleep(1)
